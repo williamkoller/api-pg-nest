236d4604bdb2032dcd6e4da6dbd97bb391b8834d
William Koller <wkoller25@gmail.com>
2020-12-05 01:03:46 -0300

    fix: remove semicolon

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { TypeORMConfig } from './configs/typeorm.config'
 import { UsersModule } from './users/users.module'
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module'
 
 @Module({
     imports: [TypeOrmModule.forRoot(TypeORMConfig), UsersModule, AuthModule],
