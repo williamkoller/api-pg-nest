a27cf391494f1f873c18177f066cc1bb4d9a0c02
William Koller <wkoller25@gmail.com>
2020-12-07 01:36:05 -0300

    feat: add method resetPassword

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,21 @@
-import { Body, Controller, Get, Post, UseGuards, ValidationPipe } from '@nestjs/common'
+import {
+    Body,
+    Controller,
+    Get,
+    Param,
+    Patch,
+    Post,
+    UnauthorizedException,
+    UseGuards,
+    ValidationPipe,
+} from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 import { CreateUserDto } from 'src/users/dtos'
 import { User } from 'src/users/entity/user.entity'
+import { UserRole } from 'src/users/user-enum/user-role.enum'
 import { AuthService } from './auth.service'
 import { GetUser } from './decorator/get-user.decorator'
+import { ChangePasswordDto } from './dtos/change-password.dto'
 import { CredentialsDto } from './dtos/credentials-dto'
 
 @Controller('api/v1/auth')
@@ -21,6 +33,36 @@ export class AuthController {
     @Post('/signin')
     async signIn(@Body(ValidationPipe) credentialsDto: CredentialsDto): Promise<{ token: string }> {
         return this.authService.signIn(credentialsDto)
+    }
+
+    @Patch('/update/token/:token')
+    async confirmEmail(@Param('token') token: string): Promise<{ message: string }> {
+        await this.authService.confirmEmail(token)
+        return {
+            message: 'E-mail confirmation',
+        }
+    }
+
+    @Post('/send-recover-email')
+    async sendRecoverPasswordEmail(@Body('email') email: string): Promise<{ message: string }> {
+        await this.authService.sendRecoverPasswordEmail(email)
+        return {
+            message: 'An email has been sent with instructions to reset your password.',
+        }
+    }
+
+    @Patch('/id:/reset-password')
+    async resetPassword(
+        @Param('id') id: string,
+        @Body(ValidationPipe) changePasswordDto: ChangePasswordDto,
+        @GetUser() user: User,
+    ): Promise<{ message: string }> {
+        if (user.role !== UserRole.ADMIN && user.id.toString() !== id)
+            throw new UnauthorizedException('You are not allowed to perform this operation.')
+        await this.authService.resetPassword(id, changePasswordDto)
+        return {
+            message: 'Password updated successfully.',
+        }
     }
 
     @Get('/me')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,19 @@
-import { Injectable, UnauthorizedException, UnprocessableEntityException } from '@nestjs/common'
+import { MailerService } from '@nestjs-modules/mailer'
+import {
+    Injectable,
+    NotAcceptableException,
+    NotFoundException,
+    UnauthorizedException,
+    UnprocessableEntityException,
+} from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { InjectRepository } from '@nestjs/typeorm'
+import { randomBytes } from 'crypto'
 import { CreateUserDto } from 'src/users/dtos'
 import { User } from 'src/users/entity/user.entity'
 import { UserRepository } from 'src/users/repository/users.repository'
 import { UserRole } from 'src/users/user-enum/user-role.enum'
+import { ChangePasswordDto } from './dtos/change-password.dto'
 import { CredentialsDto } from './dtos/credentials-dto'
 
 @Injectable()
@@ -13,13 +22,26 @@ export class AuthService {
         @InjectRepository(UserRepository)
         private userRepository: UserRepository,
         private jwtService: JwtService,
+        private mailerService: MailerService,
     ) {}
 
     async signUp(createdUserDto: CreateUserDto): Promise<User> {
         if (createdUserDto.password != createdUserDto.passwordConfirmation) {
             throw new UnprocessableEntityException('The passwods not matchs.')
         } else {
-            return await this.userRepository.createUser(createdUserDto, UserRole.USER)
+            const user = await this.userRepository.createUser(createdUserDto, UserRole.USER)
+
+            const mail = {
+                to: user.email,
+                from: 'williamkoller404@gmail.com',
+                subject: 'Email de confirmação',
+                template: 'email-confirmation',
+                context: {
+                    token: user.confirmationToken ? user.confirmationToken : null,
+                },
+            }
+            await this.mailerService.sendMail(mail)
+            return user
         }
     }
 
@@ -37,6 +59,64 @@ export class AuthService {
 
         return {
             token,
+        }
+    }
+
+    async confirmEmail(confirmationToken: string): Promise<void> {
+        const result = await this.userRepository.update(
+            {
+                confirmationToken,
+            },
+            {
+                confirmationToken: null,
+            },
+        )
+        if (result.affected === 0) throw new NotAcceptableException('token invalid.')
+    }
+
+    async sendRecoverPasswordEmail(email: string): Promise<void> {
+        const user = await this.userRepository.findOne({ email })
+        if (!user) {
+            throw new NotFoundException('There is no user found with this email.')
+        }
+
+        user.recoverToken = randomBytes(32).toString('hex')
+        await user.save()
+
+        const mail = {
+            to: user.email,
+            from: 'williamkoller404@gmail.com',
+            subject: 'Recuperação de senha',
+            template: 'recover-password',
+            context: {
+                token: user.recoverToken,
+            },
+        }
+        await this.mailerService.sendMail(mail)
+    }
+
+    async changePassword(id: string, changePasswordDto: ChangePasswordDto): Promise<void> {
+        const { password, passwordConfirmation } = changePasswordDto
+        if (password != passwordConfirmation) {
+            throw new UnprocessableEntityException('The passwords not matchs.')
+        }
+        await this.userRepository.changePassword(id, password)
+    }
+
+    async resetPassword(recoverToken: string, changePasswordDto: ChangePasswordDto): Promise<void> {
+        const user = await this.userRepository.findOne(
+            {
+                recoverToken: recoverToken ? recoverToken : null,
+            },
+            {
+                select: ['id'],
+            },
+        )
+        if (!user) throw new NotFoundException('Token invalid.')
+        try {
+            await this.changePassword(user.id.toString(), changePasswordDto)
+        } catch (error) {
+            throw error
         }
     }
 }
