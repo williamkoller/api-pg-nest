2c6318fbf20aedcc2e02ba28d7e0e6a671f836d3
William Koller <wkoller25@gmail.com>
2020-12-07 01:33:24 -0300

    feat: add MailerModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,17 @@ import { LoggerInterceptor } from './interceptors/logger.interceptor'
 import { APP_INTERCEPTOR } from '@nestjs/core'
 import { WinstonModule } from 'nest-winston'
 import { winstonConfig } from './configs/winston.config'
+import { MailerModule } from '@nestjs-modules/mailer'
+import { mailerConfig } from './configs/mailer.config'
 
 @Module({
-    imports: [TypeOrmModule.forRoot(TypeORMConfig), WinstonModule.forRoot(winstonConfig), UsersModule, AuthModule],
+    imports: [
+        TypeOrmModule.forRoot(TypeORMConfig),
+        WinstonModule.forRoot(winstonConfig),
+        MailerModule.forRoot(mailerConfig),
+        UsersModule,
+        AuthModule,
+    ],
     controllers: [],
     providers: [
         {
